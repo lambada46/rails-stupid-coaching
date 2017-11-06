6876cfc6c4e6a624a36a1a5b32f7df590342ae40
lucas furtado <lambada46@gmail.com>
2017-11-06 12:27:44 -0200

    conditional statements modifying the coach answer in the controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,18 @@
 class QuestionsController < ApplicationController
   def answer
+    @ask = params[:query]
+    @message = ''
+    if @ask  == "I am going to work right now!"
+    @message = 'Good job!'
+    elsif @ask.include? "?"
+    @message = 'Silly question, get dressed and go to work!'
+    else
+    @message = "I don't care, get dressed and go to work!"
+   end
   end
 
   def ask
   end
 end
+
+
